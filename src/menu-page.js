50ab37582d87acee9584fc920ddda0124b14c584
71M <71m8ay@gmail.com>
2024-10-31 20:44:31 +0400

    added Lorem Ipsum library

---
+++ src/menu-page.js
@@ -0,0 +1,27 @@
+import { LoremIpsum } from "lorem-ipsum";
+
+const lorem = new LoremIpsum();
+
+export function menuPage() {
+  const content = document.querySelector("#content");
+  content.textContent = "";
+  content.className = "menuPage";
+
+  document.querySelector("nav > button:first-child").className = "homeBgColor";
+  document.querySelector("nav > button:last-child").className = "aboutBgColor";
+
+  const menuButton = document.querySelector("button:nth-child(2)");
+  menuButton.className = "menuButton";
+
+  const menuTitle = document.createElement("p");
+  menuTitle.textContent = "Menu: ";
+
+  const menuDiv = document.createElement("div");
+  menuDiv.className = "menuDiv";
+  const menuList = document.createElement("ul");
+  for (let i = 0; i < 9; i++) {
+    menuList.appendChild(Object.assign(document.createElement("li")), {
+      textContent: `${lorem.generateWords(Math.floor(Math.random() * 4) + 1)}`,
+    });
+  }
+}
